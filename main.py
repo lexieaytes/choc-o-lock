77127c0f3c707552f48935ac2343f217343662e2
lexieaytes <lexieaytes@gmail.com>
2021-02-19 11:21:05 -0500

    Add AccessToken and refactor

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from database import *
 from lock import *
 
 TIMEOUT = 10
-AUTH_THRESHOLD = 5
-MAX_OPEN_TIME = 5
+AUTH_THRESHOLD = 4
+TIME_TO_BE_CONSIDERED_UNATTENDED = 10  # seconds
+UNKNOWN_USER_THRESHOLD = 4
 
 
 def get_user_from_face(db, face):
@@ -73,13 +74,13 @@ class AuthToken:
         if users_are_authorized(self.db, users, self.resource_id):
             self.count += 1
             if self.count >= AUTH_THRESHOLD:
-                self.result = True  # successful authorization
-                self.done = True  
+                self.result = True  # successfully authenticated
+                self.done = True 
                 
         self.timeout -= 1  # consumed 1 chance to authenticate
 
         if self.timeout == 0:
-            self.done = True  # timed out
+            self.done = True  
 
 def everybody_in_video_is_authorized(db, kc, resource_id):
     token = AuthToken(db, resource_id)
@@ -102,45 +103,77 @@ def everybody_in_video_is_authorized(db, kc, resource_id):
     db.log_access_attempt(users, resource_id, token.result)
     return token.result
 
+class Timer:
+
+    def __init__(self, time_interval):
+        self.time_interval = time_interval
+        self.start_time = None
+
+    def start(self):
+        self.start_time = time.time()
+
+    @property
+    def is_expired(self):
+        return self.start_time and time.time() - self.start_time > self.time_interval
+
+class AccessToken:
+
+    def __init__(self, db, resource_id, initial_users):
+        self.db = db
+        self.resource_id = resource_id
+        self.curr_users = set(initial_users)
+        self.unknown_count = 0
+        self.unattended = False
+        self.unattended_timer = Timer(TIME_TO_BE_CONSIDERED_UNATTENDED)
+
+    def check(self, users):
+        if users:
+            self.unattended = False
+            self.verify(users)
+        elif not self.unattended:
+            print('nobody on screen')
+            self.unattended = True
+            self.unattended_timer.start()
+        elif self.unattended_timer.is_expired:
+            print('!!! UNATTENDED ALERT !!!')
+            self.db.log_resource_time_out(self.resource_id)
+            self.unattended_timer.start()  # restart timer
+        else:
+            print('still unattended')
+
+    def verify(self, users):
+        new_users = set(users) - self.curr_users
+        num_unknown_users = 0
+        for user in new_users:
+            if user is not Unknown:
+                authorized = user_is_authorized(self.db, user, self.resource_id)
+                self.db.log_new_user_appearance(user, self.resource_id, authorized)
+                self.curr_users.add(user)
+            else:
+                num_unknown_users += 1
+
+        print('Unknown users:', num_unknown_users)
+        print('Count:', self.unknown_count)
+
+        if num_unknown_users:
+            self.unknown_count += 1
+            if self.unknown_count == UNKNOWN_USER_THRESHOLD:
+                self.db.log_unknown_user_appearance(num_unknown_users, self.resource_id)
+        else:
+            # There were no unknown users
+            self.unknown_count = 0
+
 def access_handler(db, kc, resource_id):
     faces = kc.get_faces_in_video()
-    initial_users = set(get_users_from_faces(db, faces))
-    unattended = False
-    unattended_count = 0
-    new_user_appearances = defaultdict(int)
-    NEW_USER_APPEARANCE_THRESHOLD = 5
+    initial_users = get_users_from_faces(db, faces)
+    token = AccessToken(db, resource_id, initial_users)
 
     while resource_is_unlocked():
         faces = kc.get_faces_in_video()
+        users = get_users_from_faces(db, faces)
+        token.check(users)
 
-        if faces:
-            unattended = False
-            unattended_count = 0
-            new_users = set(get_users_from_faces(db, faces)) - initial_users
-            for user in new_users:
-                new_user_appearances[user] += 1
-                if new_user_appearances[user] == NEW_USER_APPEARANCE_THRESHOLD:
-                    # We can say with confidence that someone new has joined the party
-                    # TODO make this singular
-                    authorized = users_are_authorized(db, new_users, resource_id)
-                    db.log_new_users_appearance(new_users, resource_id, authorized)
-
-        elif unattended:
-            if (time.time() -  start_time) > MAX_OPEN_TIME:
-                # Left unattended for too long, send alert
-                db.log_resource_time_out(resource_id)
-                start_time = time.time()  # restart timer
-        elif unattended_count > 5:
-            # Now it is unattended, since we didn't see anyone on camera
-            print('UNATTENDED!!!')
-            unattended = True  
-            start_time = time.time()  # start timer
-        else:
-            print("increment unattended count")
-            unattended_count += 1
-    
-
-    db.log_resource_close(resource_id)
+    db.log_resource_close(users, resource_id)
 
 def getenv(var_name):
     # Get the value of an environment variable
@@ -148,7 +181,6 @@ def getenv(var_name):
     if val is None:
         raise ValueError(f"environment variable '{var_name}' not set")
     return val
-
 
 def run_main_auth_routine():
     resource_id = getenv('CHOC_O_LOCK_RESOURCE_ID')
